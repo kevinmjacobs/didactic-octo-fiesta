a597183d5a8b416752e6026e44ecb17087ad354a
Kevin Jacobs <frevin8r@gmail.com>
2018-07-07 09:29:37 -0700

    move functions to helper file

--- app.js
+++ app.js
@@ -1,21 +1,14 @@
+const { isDiagonalWin, isHorizontalWin, isVerticalWin } = require('./helpers.js')
+
 let board = {
-  1: 'X',
+  1: '',
   2: '',
   3: '',
-  4: 'X',
+  4: '',
   5: '',
   6: '',
-  7: 'X', 
+  7: '', 
   8: '',
   9: ''
 }
 
-const isVerticalWin = (board) => {
-  return (((board[1] === board[4] && board[1] === board[7] && board[1] !== '') || (board[2] === board[5] && board[2] === board[8] && board[2] !== '') || (board[3] === board[6] && board[3] === board[9] && board[3] !== '')) ? true : false)
-}
-
-console.log(isVerticalWin(board));
-
-const isHorizontalWin = (board) => {
-
-}
