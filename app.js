966e605edd0d405b8a56364fd200959c53bbc68c
Kevin Jacobs <frevin8r@gmail.com>
2018-07-07 10:04:00 -0700

    set up input/output for users, set up checks if entries are valid and by which player

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const readline = require('readline')
 const { isDiagonalWin, isHorizontalWin, isVerticalWin } = require('./helpers.js')
 
 let board = {
@@ -12,12 +13,40 @@ let board = {
   C3: ''
 }
 
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
 console.log('Welcome to Tic-Tac-Toe!');
 
 const ticTacToeGame = (board, player) => {
   console.log(`${board.A1} | ${board.A2} | ${board.A3} \n${board.B1} | ${board.B2} | ${board.B3} \n${board.C1} | ${board.C2} | ${board.C3}`);
   if ((!isDiagonalWin(board)) && (!isHorizontalWin(board)) && (!isVerticalWin(board))) {
-
+    if (player === 1) {
+      rl.question('Player 1 please fill in a square (X):', (answer) => {
+        if (board[answer] === undefined) {
+          console.log('Not a valid square');
+          ticTacToeGame(board, player)
+        }
+        else {
+          board[answer]='X';
+          ticTacToeGame(board, 2);
+        }
+      })
+    }
+    if (player === 2) {
+      rl.question('Player 2 please fill in a square (O):', (answer) => {
+        if (board[answer] === undefined) {
+          console.log('Not a valid square');
+          ticTacToeGame(board, player)
+        }
+        else {
+          board[answer]='O';
+          ticTacToeGame(board, 1);
+        }
+      })
+    }
   }
 }
 
