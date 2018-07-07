a41bf23bca3e5358b89dfbb0336f2b0c2bbb8799
Kevin Jacobs <frevin8r@gmail.com>
2018-07-07 09:27:07 -0700

    add horizontal and diagonal win functions

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 let board = {
-  1: '',
+  1: 'X',
   2: '',
   3: '',
-  4: '',
+  4: 'X',
   5: '',
   6: '',
-  7: '', 
+  7: 'X', 
   8: '',
   9: ''
 }
 
 const isVerticalWin = (board) => {
   return (((board[1] === board[4] && board[1] === board[7] && board[1] !== '') || (board[2] === board[5] && board[2] === board[8] && board[2] !== '') || (board[3] === board[6] && board[3] === board[9] && board[3] !== '')) ? true : false)
+}
+
+console.log(isVerticalWin(board));
+
+const isHorizontalWin = (board) => {
+
 }
