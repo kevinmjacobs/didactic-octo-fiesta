9a487543bc20a0caf972e228c15387817484eb18
Kevin Jacobs <frevin8r@gmail.com>
2018-07-07 09:48:19 -0700

    update board object reference

--- app.js
+++ app.js
@@ -15,12 +15,11 @@ let board = {
 console.log('Welcome to Tic-Tac-Toe!');
 
 const ticTacToeGame = (board, player) => {
-  console.log(`${board[A1]} | ${board[A2]} | ${board[A3]} \n${board[B1]} | ${board[B2]} | ${board[B3]} \n${board[C1]} | ${board[C2]} | ${board[C3]}`);
+  console.log(`${board.A1} | ${board.A2} | ${board.A3} \n${board.B1} | ${board.B2} | ${board.B3} \n${board.C1} | ${board.C2} | ${board.C3}`);
   if ((!isDiagonalWin(board)) && (!isHorizontalWin(board)) && (!isVerticalWin(board))) {
 
   }
 }
-
 
 ticTacToeGame(board, 1);
 
--- helpers.js
+++ helpers.js
@@ -1,13 +1,13 @@
 const isVerticalWin = (board) => {
-  return (((board[A1] === board[B1] && board[A1] === board[C1] && board[A1] !== '') || (board[A2] === board[B2] && board[A2] === board[C2] && board[A2] !== '') || (board[A3] === board[B3] && board[A3] === board[C3] && board[A3] !== '')) ? true : false);
+  return (((board.A1 === board.B1 && board.A1 === board.C1 && board.A1 !== '') || (board.A2 === board.B2 && board.A2 === board.C2 && board.A2 !== '') || (board.A3 === board.B3 && board.A3 === board.C3 && board.A3 !== '')) ? true : false);
 }
 
 const isHorizontalWin = (board) => {
-  return (((board[A1] === board[A2] && board[A1] === board[A3] && board[A1] !== '') || (board[B1] === board[B2] && board[B1] === board[B3] && board[B1] !== '') || (board[C1] === board[C2] && board[C1] === board[C3] && board[C1] !== '')) ? true : false);
+  return (((board.A1 === board.A2 && board.A1 === board.A3 && board.A1 !== '') || (board.B1 === board.B2 && board.B1 === board.B3 && board.B1 !== '') || (board.C1 === board.C2 && board.C1 === board.C3 && board.C1 !== '')) ? true : false);
 }
 
 const isDiagonalWin = (board) => {
-  return (((board[A1] === board[B2] && board[A1] === board[C3] && board[A1] !== '') || (board[A3] === board[B2] && board[A3] === board[C1] && board[A3] !== '')) ? true : false);
+  return (((board.A1 === board.B2 && board.A1 === board.C3 && board.A1 !== '') || (board.A3 === board.B2 && board.A3 === board.C1 && board.A3 !== '')) ? true : false);
 }
 
 module.exports = {
